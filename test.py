461f9fe771fb527f3aafb1a74980228bfa4bb8ac
Adarsh Tayde <102159256+Adarsh-365@users.noreply.github.com>
2024-09-09 18:23:48 +0530

    Update test.py
    DIC

--- test.py
+++ test.py
@@ -29,7 +29,7 @@ for tras in data:
 # itemset = []
 # itemset_count = {}
 # freq_itemset = []
-min_sup =4 
+min_sup = 2
 m = 2
 total_trans =len(Dict1)
 
@@ -42,121 +42,110 @@ count = 0
 list_of_key = [key for key in Dict1]
 a = 0
 b = 2
-
+k = 1
 itemset_count = {}
 
-while(count!=total_trans):
-    Dict2 = {}
-    for key in list_of_key[a:b]:
-        trans = Dict1[key]
-        for item in trans:
-            if item not in DC:
-                DC.append(item)
-                itemset_count[str(item)]=1
-            else:
-                itemset_count[str(item)]+=1
-            
-            if itemset_count[str(item)]>=min_sup:
-                if item not in DS:
-                    DS.append(item)
-                    #DC.remove(item)
-            
+frequent_itemset = []
+while(len(DC)!=0 or k==1):
+    
+    while(count!=total_trans):
+        Dict2 = {}
+        if k==1:
+            for key in list_of_key[a:b]:
+                trans = Dict1[key]
+                for item in trans:
+                    if item not in DC:
+                        DC.append(item)
+                        itemset_count[str(item)]=1
+                    else:
+                        itemset_count[str(item)]+=1
+                    
+                    if itemset_count[str(item)]>=min_sup:
+                        
+                        if item not in DS:
+                            DS.append(item)
+                            #DC.remove(item)
+        
+        else:
            
+            for item in DC:
+                for key in list_of_key[a:b]:
+                    trans = Dict1[key]
+                    sbc = np.all(np.isin(item,trans))
+                    
+                    if sbc:
+                    # print(itemset_count)
+                        if str(item) not in itemset_count:
+                            itemset_count[str(item)]=1
+                        else:
+                            itemset_count[str(item)]+=1
+                # for item in DC:
+                    
+                        print(itemset_count)
+                        if itemset_count[str(item)]>=min_sup:
+                            
+                                if not any(np.array_equal(item,arr) for arr in DS):
+                                
+                                    DS.append(item)
+                            
+                    
                 
+                    
+                    
+                
+                    
+                
+                        
+                 
+        count+=m
+        a=b
+        b=b+m
+    
+    k = k+1   
+    count = 0
+    a = 0
+    b = 2  
+    SC =  []
+    
+    SS = []
+    
+    for item in DC:
+        
+        try:
+             if item not in DS:
             
-    count+=m
-    a=b
-    b=b+m
-
-    if count==total_trans:
-        for item in DC:
-            if item not in DS:
                 SC.append(item)
-              #  DC.remove(item)
-        for item in DS:
-            SS.append(item)
-        DC = []
-        for i in range(len(SS)-1):
-            for j in range(i+1,SS):
-                set1 = np.union1d(SS[i],SS[j])
-                DC.append()
-                
+        except:
+            is_y_in_x = any(np.array_equal(item, arr) for arr in DC)
+            if not is_y_in_x:
+                print("a====>",item)
+                SC.append(item)
+           
+
+        #  DC.remove(item)
+    for item in DS:
+        SS.append(item)
+    if len(SS):
+        frequent_itemset.append(SS)
+    DC = []
+    DS = []
+    itemset_count = {}
+    for i in range(len(SS)-1):
+        for j in range(i+1,len(SS)):
+            set1 = np.union1d(SS[i],SS[j])
+            if not any(np.array_equal(set1,arr)for arr in DC):
+                 DC.append(set1)
+
+                    
             
 print("==========")        
 print("SC = ",SC)         
 
 
 print("DC = ",DC)
-print("DS = ",DS)
+print("frequent_itemset = ",frequent_itemset)
 print("SS = ",SS)  
 print(itemset_count)
 
 
 
-
-
-
-
-# for key in Dict1:
-#     trans = Dict1[key]
-#     for item in trans:
-#         if item not in itemset:
-#             itemset.append(item)
-#             itemset_count[str(item)]=1
-#         else:
-#             itemset_count[str(item)]+=1
-            
-
-# itemset_count = dict(sorted(itemset_count.items(),key=lambda item:item[1],reverse=1))
-
-# print(itemset_count)
-
-
-
-
-# for key in itemset_count:
-#     if itemset_count[key] >= min_sup:
-#         SS.append(np.array([key]))
-# print(SS)
-
-
-# for key in itemset_count:
-#     if itemset_count[key]>2:
-#         freq_itemset.append(np.array([key]))
-
-
-
-# C1 = itemset
-# Result = []
-
-# k = 1 
-
-# while(len(C1) !=0):
-#     itemset_count = {}
-#     itemset = []
-#     Fk = []
-#     if k==1:
-       
-#         Fk = freq_itemset
-#     else:
-#         for key in Dict1:
-#             for item in C1:
-#                 subset =np.all(np.isin(np.array(item),np.array(Dict1[key])))
-#                 if subset:
-#                     if str(item) not in itemset_count:
-#                         itemset_count[str(item)]=1
-#                         itemset.append(np.array(item))
-#                     else:
-#                         itemset_count[str(item)]+=1
-#         for i,item in enumerate(itemset_count):
-#             if itemset_count[str(item)]>=min_sup:
-#                 Fk.append(np.array(itemset[i]))
-    
-   
-#     C1 = Candidate_generate(Fk,k)
-   
-#     if len(Fk):
-#         Result.append(Fk)
-#     k=k+1
-
-# print(Result)
